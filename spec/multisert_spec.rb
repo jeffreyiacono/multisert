5bc3a68844541523b39961cd52dfcf4db02da527
Jeff Iacono <iacono@squareup.com>
2013-03-27 11:45:54 -0700

    Update test to demonstrate overwrite + insert of content

--- spec/multisert_spec.rb
+++ spec/multisert_spec.rb
@@ -7,12 +7,22 @@ TEST_DATABASE      = 'multisert_test'
 TEST_TABLE         = 'test_data'
 TEST_INDEXED_TABLE = 'test_indexed_data'
 
+def test_table
+  "#{TEST_DATABASE}.#{TEST_TABLE}"
+end
+
+def test_indexed_table
+  "#{TEST_DATABASE}.#{TEST_INDEXED_TABLE}"
+end
+
 # TODO: make into yaml config
 $connection = Mysql2::Client.new(host: 'localhost', username: 'root')
 
-$cleaner = MultisertSpec::MrClean.new(database: TEST_DATABASE, connection: $connection) do |mgr|
+$cleaner = MultisertSpec::MrClean.new(database: TEST_DATABASE,
+                                      connection: $connection) do |mgr|
+
   mgr.create_table_schemas << %[
-    CREATE TABLE IF NOT EXISTS #{mgr.database}.#{TEST_TABLE} (
+    CREATE TABLE IF NOT EXISTS #{test_table} (
       test_field_int_1 int default null,
       test_field_int_2 int default null,
       test_field_int_3 int default null,
@@ -22,7 +32,7 @@ $cleaner = MultisertSpec::MrClean.new(database: TEST_DATABASE, connection: $conn
       test_field_datetime DATETIME default null)]
 
   mgr.create_table_schemas << %[
-    CREATE TABLE IF NOT EXISTS #{mgr.database}.#{TEST_INDEXED_TABLE} (
+    CREATE TABLE IF NOT EXISTS #{test_indexed_table} (
       test_id int not null,
       test_field varchar(15) default null,
       primary key (test_id))]
@@ -54,17 +64,17 @@ describe Multisert do
     end
 
     it "does not fall over when there are no entries" do
-      connection.query "DELETE FROM #{TEST_DATABASE}.#{TEST_TABLE}"
+      connection.query "DELETE FROM #{test_table}"
 
       buffer.write_buffer!
 
-      write_buffer_records = connection.query "SELECT * FROM #{TEST_DATABASE}.#{TEST_TABLE}"
+      write_buffer_records = connection.query "SELECT * FROM #{test_table}"
       expect(write_buffer_records.to_a).to eq []
       expect(buffer.entries).to eq []
     end
 
     it "multi-inserts all added entries and clears #entries" do
-      pre_write_buffer_records = connection.query "SELECT * FROM #{TEST_DATABASE}.#{TEST_TABLE}"
+      pre_write_buffer_records = connection.query "SELECT * FROM #{test_table}"
       expect(pre_write_buffer_records.to_a).to eq []
 
       buffer.connection = connection
@@ -88,7 +98,7 @@ describe Multisert do
           , test_field_int_2
           , test_field_int_3
           , test_field_int_4
-        FROM #{TEST_DATABASE}.#{TEST_TABLE}]
+        FROM #{test_table}]
 
       expect(post_write_buffer_records.to_a).to eq [
         {'test_field_int_1' => 1,  'test_field_int_2' => 3,  'test_field_int_3' => 4,  'test_field_int_4' => 5},
@@ -100,7 +110,7 @@ describe Multisert do
     end
 
     it "works with strings" do
-      pre_write_buffer_records = connection.query "SELECT * FROM #{TEST_DATABASE}.#{TEST_TABLE}"
+      pre_write_buffer_records = connection.query "SELECT * FROM #{test_table}"
       expect(pre_write_buffer_records.to_a).to eq []
 
       buffer.connection = connection
@@ -115,7 +125,7 @@ describe Multisert do
 
       buffer.write_buffer!
 
-      post_write_buffer_records = connection.query %[SELECT test_field_varchar FROM #{TEST_DATABASE}.#{TEST_TABLE}]
+      post_write_buffer_records = connection.query %[SELECT test_field_varchar FROM #{test_table}]
       expect(post_write_buffer_records.to_a).to eq [
         {'test_field_varchar' => 'a'},
         {'test_field_varchar' => 'b'},
@@ -128,7 +138,7 @@ describe Multisert do
     it "works with strings that have illegal characters"
 
     it "works with dates" do
-      pre_write_buffer_records = connection.query "SELECT * FROM #{TEST_DATABASE}.#{TEST_TABLE}"
+      pre_write_buffer_records = connection.query "SELECT * FROM #{test_table}"
       expect(pre_write_buffer_records.to_a).to eq []
 
       buffer.connection = connection
@@ -143,7 +153,7 @@ describe Multisert do
 
       buffer.write_buffer!
 
-      post_write_buffer_records = connection.query %[SELECT test_field_date FROM #{TEST_DATABASE}.#{TEST_TABLE}]
+      post_write_buffer_records = connection.query %[SELECT test_field_date FROM #{test_table}]
 
       expect(post_write_buffer_records.to_a).to eq [
         {'test_field_date' => Date.parse('2013-01-15')},
@@ -155,7 +165,7 @@ describe Multisert do
     end
 
     it "works with times" do
-      pre_write_buffer_records = connection.query "SELECT * FROM #{TEST_DATABASE}.#{TEST_TABLE}"
+      pre_write_buffer_records = connection.query "SELECT * FROM #{test_table}"
       expect(pre_write_buffer_records.to_a).to eq []
 
       buffer.connection = connection
@@ -170,7 +180,7 @@ describe Multisert do
 
       buffer.write_buffer!
 
-      post_write_buffer_records = connection.query %[SELECT test_field_datetime FROM #{TEST_DATABASE}.#{TEST_TABLE}]
+      post_write_buffer_records = connection.query %[SELECT test_field_datetime FROM #{test_table}]
 
       expect(post_write_buffer_records.to_a).to eq [
         {'test_field_datetime' => Time.new(2013, 1, 15, 1, 5, 11)},
@@ -208,7 +218,7 @@ describe Multisert do
 
     context "set to replace" do
       it "writes over an existing record with the same primary / unique key" do
-        connection.query %[INSERT INTO #{TEST_DATABASE}.#{TEST_INDEXED_TABLE} (test_id, test_field)
+        connection.query %[INSERT INTO #{test_indexed_table} (test_id, test_field)
                            VALUES (1, 'ONE'), (2, 'TWO')]
 
         buffer.connection      = connection
@@ -226,7 +236,7 @@ describe Multisert do
           SELECT
               test_id
             , test_field
-          FROM #{TEST_DATABASE}.#{TEST_INDEXED_TABLE}]
+          FROM #{test_indexed_table}]
 
         expect(post_write_buffer_records.to_a).to eq [
           {'test_id' => 1, 'test_field' => 'SOMETHING NEW'},
@@ -237,7 +247,7 @@ describe Multisert do
 
     context "set to ignore" do
       before do
-        connection.query %[INSERT INTO #{TEST_DATABASE}.#{TEST_INDEXED_TABLE} (test_id, test_field)
+        connection.query %[INSERT INTO #{test_indexed_table} (test_id, test_field)
                            VALUES (1, 'ONE'), (2, 'TWO')]
 
         buffer.connection      = connection
@@ -261,7 +271,7 @@ describe Multisert do
           SELECT
               test_id
             , test_field
-          FROM #{TEST_DATABASE}.#{TEST_INDEXED_TABLE}]
+          FROM #{test_indexed_table}]
 
         expect(post_write_buffer_records.to_a).to eq [
           {'test_id' => 1, 'test_field' => 'ONE'},
